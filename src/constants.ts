9f9aaa19530bcfdd3f6a60636ee485ab7367968b
Antonio Morrone <antonio@iovlabs.org>
2021-08-05 11:02:19 +0200

    Add RSK mainnet addresses

--- src/constants.ts
+++ src/constants.ts
@@ -7,7 +7,6 @@ export const TESTNET_SAFE_DEPLOYMENT = {
 }
 
 export const MAINNET_SAFE_DEPLOYMENT = {
-  // FIXME: to be defined
-  safeSingletonAddress: '0xffd41b816f2821e579b4da85c7352bf4f17e4fa5',
-  proxyFactoryAddress: '0x5b836117aed4ca4dee8e2e464f97f7f59b426c5a'
+  safeSingletonAddress: '0xc6cfa90ff601d6aac45d8dcf194cf38b91aca368',
+  proxyFactoryAddress: '0x4b1af52ea200baebf79450dbc996573a7b75f65a'
 }
